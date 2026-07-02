fea53d733131cc10e204216b30a1ab6c3d81c46a
Divcroot <divyanshu9984chauhan@gmail.com>
2026-07-02 19:44:11 +0530

    fixing errors

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -8,16 +8,16 @@ import openai from "../configs/openai.js";
 export const textMessageController = async (req, res) => {
     try {
         const userId = req.user._id;
-        if(req.user.credits < 1){
-            return res.json({success: false, message: "You don't have enough credits to use this feature"})
+        if (req.user.credits < 1) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
         }
 
-        const { chatId, prompt } = req.body; 
+        const { chatId, prompt } = req.body;
 
         const chat = await Chat.findOne({ userId, _id: chatId });
         chat.messages.push({ role: 'user', content: prompt, timestamp: Date.now(), isImage: false })
 
-        const {choices} = await openai.chat.completions.create({
+        const { choices } = await openai.chat.completions.create({
             model: "gemini-2.0-flash",
             messages: [
                 {
@@ -27,20 +27,22 @@ export const textMessageController = async (req, res) => {
             ],
         });
 
-        const reply = {...choices[0].message, timestamp: Date.now(), isImage: false}
-        res.json({success: true, reply})
+        const reply = { ...choices[0].message, timestamp: Date.now(), isImage: false }
+        res.json({ success: true, reply })
 
         chat.messages.push(reply)
         await chat.save()
-        await User.updateOne({_id: userId}, {$inc: {credits: -1}})
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } })
 
     } catch (error) {
-        console.log(error)
-        if(error.response){
-            console.log(error.response.data)
-            console.log(error.message)
-        }
-        res.status(500).json({success: false, message: error.message})
+        console.dir(error, { depth: null });
+
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+            status: error.status,
+            cause: error.cause,
+        });
     }
 }
 
@@ -49,20 +51,21 @@ export const imageMessageController = async (req, res) => {
     try {
         const userId = req.user._id;
         //Check credits
-        if(req.user.credits < 2){
-            return res.json({success: false, message: "You don't have enough credits to use this feature"})
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
         }
-        const {prompt, chatId, isPublished} = req.body;
+        const { prompt, chatId, isPublished } = req.body;
         // Find chat
-        const chat = await Chat.findOne({userId, _id: chatId})
+        const chat = await Chat.findOne({ userId, _id: chatId })
 
         //Push user message
         chat.messages.push({
-            role: 'user', 
-            content: prompt, 
-            timestamp: Date.now(), 
-            isImage: false});
-        
+            role: 'user',
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false
+        });
+
         // Encode the prompt
         const encodedPrompt = encodeURIComponent(prompt);
 
@@ -70,7 +73,7 @@ export const imageMessageController = async (req, res) => {
         const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/QuickGPT/${Date.now()}.png?tr=w-800,h-800`;
 
         // Trigger generation by fetching from ImageKit
-        const aiImageResponse = await axios.get(generatedImageUrl, {responseType: "arraybuffer"})
+        const aiImageResponse = await axios.get(generatedImageUrl, { responseType: "arraybuffer" })
 
         //Convert to base64
         const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString('base64')}`;
@@ -90,13 +93,13 @@ export const imageMessageController = async (req, res) => {
             isPublished
         }
 
-        res.json({success: true, reply})
+        res.json({ success: true, reply })
 
         chat.messages.push(reply)
         await chat.save()
-        await User.updateOne({_id: userId}, {$inc: {credits: -2}})
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } })
 
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
